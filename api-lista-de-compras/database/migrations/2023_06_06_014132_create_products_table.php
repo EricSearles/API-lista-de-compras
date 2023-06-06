4310468fe84ab0655edd3a540c4ed2b9956fda53
EricSearles <ericsearles@gmail.com>
2023-06-05 23:07:11 -0300

    Definindo campos para tabelas

--- api-lista-de-compras/database/migrations/2023_06_06_014132_create_products_table.php
+++ api-lista-de-compras/database/migrations/2023_06_06_014132_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedInteger('quantity');
+            $table->unsignedBigInteger('shopping_list_id');
+            $table->foreign('shopping_list_id')->references('id')->on('shopping_lists')->onDelete('cascade');
             $table->timestamps();
         });
     }
