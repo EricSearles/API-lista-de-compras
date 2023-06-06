61aac5f0f7195afa914a2d4e263415e4c9212818
EricSearles <ericsearles@gmail.com>
2023-06-05 23:46:33 -0300

    Criando rotas

--- api-lista-de-compras/routes/api.php
+++ api-lista-de-compras/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ShoppingListController;
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Rotas disponiveis sem autenticação de usuario
+Route::prefix('shopping-lists')->group(function () {
+    Route::post('/', [ShoppingListController::class, 'create']);
+    Route::post('/{shoppingListId}/products', [ShoppingListController::class, 'addProduct']);
+    Route::delete('/{shoppingListId}/products/{productId}', [ShoppingListController::class, 'removeProduct']);
+    Route::get('/{shoppingListId}', [ShoppingListController::class, 'getShoppingList']);
+    Route::put('/products/{productId}/increase-quantity', [ShoppingListController::class, 'increaseProductQuantity']);
+    Route::put('/products/{productId}/decrease-quantity', [ShoppingListController::class, 'decreaseProductQuantity']);
+    Route::post('/{shoppingListId}/duplicate', [ShoppingListController::class, 'duplicateShoppingList']);
+});
+
+Route::prefix('products')->group(function () {
+    Route::post('/', [ProductController::class, 'create']);
+    // Implementar outras rotas para produto.
 });
